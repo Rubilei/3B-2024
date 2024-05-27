90dfb7b6a9bf8b499094bb20a8700e2dae39da6e
Rubilei <145511175+Rubilei@users.noreply.github.com>
2024-05-27 17:59:30 -0300

    console

--- main.js
+++ main.js
@@ -1,3 +1,9 @@
 //criando a passagem entre os botões ativos ao passar o mouse
 const botoes = document.querySelectorAll(".botao");
 console.log(botoes);
+for(let i=0; i <botoes.length;i++){
+    botoes [i].onclick =function(){
+        botoes[i].classList.add("ativo");
+    }
+    console.log()
+}
