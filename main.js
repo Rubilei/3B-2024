7e87fea33543454bfa0c2f822fd5570d5ffb9ffd
Rubilei <145511175+Rubilei@users.noreply.github.com>
2024-06-05 19:11:31 -0300

    testebotoes

--- main.js
+++ main.js
@@ -1,11 +1,13 @@
 //criando a passagem entre os botões ativos ao passar o mouse
 const botoes = document.querySelectorAll(".botao");
+const textos = document.querySelectorAll(".aba-conteudo");
 
 for(let i=0; i <botoes.length;i++){
     botoes [i].onclick =function(){
         
         for(let j=0;j<botoes.length;j++){
             botoes [j].classList.remove("ativo");
+            textos [j].classList.remove("ativo");
         }
 
         botoes[i].classList.add("ativo");
