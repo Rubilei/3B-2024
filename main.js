24809f4ff4614a997a47bc27a027688b875ac8ce
Rubilei <145511175+Rubilei@users.noreply.github.com>
2024-06-05 19:33:31 -0300

    tempo

--- main.js
+++ main.js
@@ -15,7 +15,7 @@ for(let i=0; i <botoes.length;i++){
     }  
 }
 const contadores = document.querySelectorAll(".contador");
-const tempoObjetivo1 = new Date("2024-06-05T00:00:00");
+const tempoObjetivo1 = new Date("2024-12-05T00:00:00");
 let tempoAtual = new Date();
 
 contadores[0].textContent = tempoObjetivo1 - tempoAtual;
